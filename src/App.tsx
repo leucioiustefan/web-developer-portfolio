37c7b56d2ee6d4ad7f874fce8dbc7ebb85a230a6
Leucioiu Stefan <leucioiu.stefan@yahoo.com>
2021-12-27 12:48:34 +0200

    added boilerplate for portfolio page

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Logo } from './Components/Atoms/Logo/Logo';
+
+const App = () => {
+  return (
+    <div>
+      <Logo />
+    </div>
+  );
+};
+
+export default App;
