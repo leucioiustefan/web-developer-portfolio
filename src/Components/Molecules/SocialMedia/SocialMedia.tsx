78d927dcbd3f8677eb2e19f1e3171de1eb57e3f5
Leucioiu Stefan <leucioiu.stefan@yahoo.com>
2021-12-30 14:04:29 +0200

    Finished adding social media navigation

--- src/Components/Molecules/SocialMedia/SocialMedia.tsx
+++ src/Components/Molecules/SocialMedia/SocialMedia.tsx
@@ -1,12 +1,29 @@
+import { useState, useEffect } from 'react';
 import { Facebook } from '../../Atoms/Facebook/Facebook';
 import { Github } from '../../Atoms/Github/Github';
 import { Linkedin } from '../../Atoms/Linkedin/Linkedin';
 import * as S from './social-media.styles';
 
 const SocialMedia = () => {
+  const [timer, setTimer] = useState<number>(2);
+
+  //Timer with interval cleanup on dismount
+  useEffect(() => {
+    const interval: any = setInterval(() => {
+      if (timer) {
+        const next = timer - 1;
+        if (next === 0) return setTimer(0);
+        return setTimer(next);
+      }
+      return clearInterval(interval);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [timer]);
+
   return (
     <S.List>
-      <S.ListItem>
+      <S.FollowMe>Follow me</S.FollowMe>
+      <S.ListItem slideRight={timer}>
         <S.Anchor
           href='https://www.facebook.com/stefan.leucioiu.1/'
           rel='noreferrer'
@@ -15,7 +32,7 @@ const SocialMedia = () => {
           <Facebook />
         </S.Anchor>
       </S.ListItem>
-      <S.ListItem>
+      <S.ListItem slideRight={timer}>
         <S.Anchor
           href='https://github.com/leucioiustefan'
           rel='noreferrer'
@@ -24,7 +41,7 @@ const SocialMedia = () => {
           <Github />
         </S.Anchor>
       </S.ListItem>
-      <S.ListItem>
+      <S.ListItem slideRight={timer}>
         <S.Anchor
           href='https://www.linkedin.com/in/stefan-leucioiu-32b489164/'
           rel='noreferrer'
